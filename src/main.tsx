e9da80c969e1194a57b6590590017b8dc5420041
Pavlo <Dekrov.dev@gmail.com>
2026-09-15 23:05:23 +0200

    fix: wrap app in TooltipProvider and fix script path in index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Toaster } from './components/ui/sonner.tsx'
+import { TooltipProvider } from './components/ui/tooltip.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <Toaster position="bottom-right" richColors />
+    <TooltipProvider delayDuration={300}>
+      <App />
+      <Toaster position="bottom-right" richColors />
+    </TooltipProvider>
   </React.StrictMode>,
 )
